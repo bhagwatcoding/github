52496b847994c5dd629cfabfd3644d1fcb7a6a3e
Nitin rathour <workinfoanalysis@gmail.com>
2022-04-26 01:46:09 +0530

    all file is updated

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log('hello');
+let header = document.createElement('header');
+let body = document.querySelector('body');
+body.insertBefore(header, body.children[0]);
+
+fetch('header.html')
+    .then(res => res.text())
+    .then(res => {
+        header.innerHTML = res;
+    }).catch(err => {
+        console.error(err);
+    });
